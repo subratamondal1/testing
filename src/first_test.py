4adc1b0fc13ac64eb2f3f4a1eb51197a323648e4
Subrata Mondal <subratasubha2@gmail.com>
2024-01-13 12:12:27 +0530

    completed first test with PyTest

--- src/first_test.py
+++ src/first_test.py
@@ -2,5 +2,7 @@ def add(a,b):
     return a+b
 
 def divide(a,b):
+    if b==0:
+        raise ValueError
     return a/b
 
--- tests/test_first_test.py
+++ tests/test_first_test.py
@@ -6,10 +6,14 @@ def test_add():
     result=first_test.add(a=5, b=6)
     assert result==11
 
+def test_add_strings():
+    result=first_test.add(a="HI, I'm Subrata ", b="Mondal")
+    assert result=="HI, I'm Subrata Mondal"
+
 def test_divide():
     result=first_test.divide(a=10, b=5)
     assert result==2
 
 def test_divide_by_zero():
-    with pytest.raises(ZeroDivisionError):
+    with pytest.raises(ValueError):
         first_test.divide(a=10, b=0)
